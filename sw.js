d732fc7aea578d792495dac93c98fb70b3b763c6
vilaplanamaroc <vilaplana.maroc@gmail.com>
2026-02-20 05:35:45 +0000

    Delete sw.js

--- sw.js
+++
@@ -1,2 +0,0 @@
-self.addEventListener('install', e => e.waitUntil(caches.open('coran-cache').then(cache => cache.addAll(['/', '/index.html']))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(res => res || fetch(e.request))));
