cc7919a09ec2f5c1911e95b310fc39bf5336067c
vilaplanamaroc <vilaplana.maroc@gmail.com>
2026-02-20 04:20:47 +0000

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,2 @@
+self.addEventListener('install', e => e.waitUntil(caches.open('coran-cache').then(cache => cache.addAll(['/', '/index.html']))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(res => res || fetch(e.request))));
